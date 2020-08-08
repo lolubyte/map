42fe4a04e2bb5eab3e88a8edb01369652830b7df
Daniel Aboyewa <lolubyte.it@gmail.com>
2020-08-07 21:17:41 -0400

    Upadate map.js
     Change  the value of i

--- maps.js
+++ maps.js
@@ -1,7 +1,7 @@
 function maps (initPeople) {
-    let i = 0
+    let i = 2
     while (i < initPeople) {
         console.log(`${i > 0 ? 'and y' : 'Y'}ou get a map!`);
         i++
     }
-}
+}git branch --set-upstream-to=origin/master master
